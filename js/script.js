beafe0dcac763077b1b448b3a02ca966b77de656
molynatep <71908029+molynatep@users.noreply.github.com>
2021-03-18 18:36:42 -0400

    finishing up captions for pics

--- js/script.js
+++ js/script.js
@@ -1,31 +1,24 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+const modal = document.getElementById('myModal');
+const modalImg = document.getElementById("img01");
+const captionText = document.getElementById("caption");
+const images = document.getElementsByClassName('art')
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = '/images/art/portrait_large.jpg';
-    modalImg.alt = this.alt;
-    captionText.innerHTML = this.alt;
-}
-
-var img = document.getElementById('myImg2');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = '/images/art/ezra_large.jpg';
-    modalImg.alt = this.alt;
-    captionText.innerHTML = this.alt;
-}
+// Go through all of the images with our custom class
+for (let i = 0; i < images.length; i++) {
+    const img = images[i];
+    // and attach our click listener for this image.
+    img.onclick = function(evt) {
+      modal.style.display = "block";
+      modalImg.src = this.src;
+      captionText.innerHTML = this.alt;
+    }
+  }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
     modal.style.display = "none";
-}
+};
